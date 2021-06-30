fb7adc5a7d64d4681795ea93ae75084e3a1e81d4
Freya-Dixon <freya.dixon@hotmail.com>
2021-06-30 14:53:46 +0100

    added files to remove

---
+++ app.js
@@ -0,0 +1,10 @@
+// set variables on html page 
+
+
+// add event listeners to when button is clicked by user, the right return is displayed as green and the display changes to "You're correct!"
+
+
+//loop to next question, previous return finished, start next question. 
+
+
+//create reset button that returns back to start of game 
