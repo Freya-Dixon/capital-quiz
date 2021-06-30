67435c96846aa2c107e98c03c7f793b065e368dd
Freya-Dixon <freya.dixon@hotmail.com>
2021-06-30 16:27:41 +0100

    created a function to allow for question to be displayed on top of page

--- app.js
+++ app.js
@@ -3,23 +3,43 @@ const newQuestion = document.querySelector('.display')
 console.log(newQuestion);
 const questionButtons = document.querySelector('.question_btns')
 console.log(questionButtons);
+const nextButton = document.querySelector('.next')
+console.log(nextButton);
+
 // object data 
 
-const england = {
-    question: "Capital city of England is..",
-    capital:"London",
-    notCapitals: ["Glasgow", "Manchester", "Cardiff"]
+const questions = {
+    
+    "england": {
+        question: "Capital city of England is..",
+        capital: "London",
+        notCapitals: ["Glasgow", "Manchester", "Cardiff"]
+    },
+    "usa": {
+        question: "Capital city of the USA is..",
+        capital:"Washington.DC",
+        notCapitals: ["New York", "Washington", "Los Angeles"]
+    },
+    "france": {
+        question: "Capital city of France is..",
+        capital:"Paris",
+        notCapitals: ["Lille", "Lyon", "Berlin"]
+    },
+};
+console.log(questions);
+const entries = Object.entries(questions);
+const values = Object.values(questions);
+const keys = Object.keys(questions);
+console.log(values);
+console.log(entries);
+
+// 
+const getQuestion = () => {
+    newQuestion.innerHTML = values[0].question;
 }
-const usa = {
-    question: "Capital city of the USA is..",
-    capital:"Washington.DC",
-    notCapitals: ["New York", "Washington", "Los Angeles"]
-}
-const france = {
-    question: "Capital city of France is..",
-    capital:"Paris",
-    notCapitals: ["Lille", "Lyon", "Berlin"]
-}
+console.log(getQuestion);
+nextButton.addEventListener('click', getQuestion);
+
 
 // add event listeners to when button is clicked by user, the right return is displayed as green and the display changes to "You're correct!"
 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -4,22 +4,39 @@
 var newQuestion = document.querySelector('.display');
 console.log(newQuestion);
 var questionButtons = document.querySelector('.question_btns');
-console.log(questionButtons); // object data 
+console.log(questionButtons);
+var nextButton = document.querySelector('.next');
+console.log(nextButton); // object data 
 
-var england = {
-  question: "Capital city of England is..",
-  capital: "London",
-  notCapitals: ["Glasgow", "Manchester", "Cardiff"]
+var questions = {
+  "england": {
+    question: "Capital city of England is..",
+    capital: "London",
+    notCapitals: ["Glasgow", "Manchester", "Cardiff"]
+  },
+  "usa": {
+    question: "Capital city of the USA is..",
+    capital: "Washington.DC",
+    notCapitals: ["New York", "Washington", "Los Angeles"]
+  },
+  "france": {
+    question: "Capital city of France is..",
+    capital: "Paris",
+    notCapitals: ["Lille", "Lyon", "Berlin"]
+  }
 };
-var usa = {
-  question: "Capital city of the USA is..",
-  capital: "Washington.DC",
-  notCapitals: ["New York", "Washington", "Los Angeles"]
+console.log(questions);
+var entries = Object.entries(questions);
+var values = Object.values(questions);
+var keys = Object.keys(questions);
+console.log(values);
+console.log(entries); // 
+
+var getQuestion = function getQuestion() {
+  newQuestion.innerHTML = values[0].question;
 };
-var france = {
-  question: "Capital city of France is..",
-  capital: "Paris",
-  notCapitals: ["Lille", "Lyon", "Berlin"]
-}; // add event listeners to when button is clicked by user, the right return is displayed as green and the display changes to "You're correct!"
+
+console.log(getQuestion);
+nextButton.addEventListener('click', getQuestion); // add event listeners to when button is clicked by user, the right return is displayed as green and the display changes to "You're correct!"
 //loop to next question, previous return finished, start next question. 
 //create reset button that returns back to start of game
